bc1dc2cf5545cb3d7e3d42ad6cc64ba5e0df9647
latemiq <dzierzkowskij@gmail.com>
2023-05-17 12:41:23 +0200

    ADD-EMPLOYEEMANAGER-CLASS

--- src/main/java/com/github/latemiq/Employee.java
+++ src/main/java/com/github/latemiq/Employee.java
@@ -1,13 +1,13 @@
 package com.github.latemiq;
 
-public class employee {
+public class Employee {
     private String firstName;
     private String lastName;
     private String position;
     private int employeeID;
     private int salary;
 
-    public employee(String firstName, String lastName, String position, int employeeID, int salary) {
+    public Employee(String firstName, String lastName, String position, int employeeID, int salary) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.position = position;
