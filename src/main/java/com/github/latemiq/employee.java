69e9a08e72d71c91b9d432275e797bac4017327e
latemiq <dzierzkowskij@gmail.com>
2023-05-17 11:52:00 +0200

    ADD-EMPLOYEE-CONSTRUCTOR

--- src/main/java/com/github/latemiq/employee.java
+++ src/main/java/com/github/latemiq/employee.java
@@ -1,5 +1,19 @@
 package com.github.latemiq;
 
 public class employee {
-    
+    private String firstName;
+    private String lastName;
+    private String position;
+    private int employeeID;
+    private int salary;
+
+    public employee(String firstName, String lastName, String position, int employeeID, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.employeeID = employeeID;
+        this.salary = salary;
+    }
 }
+
+
