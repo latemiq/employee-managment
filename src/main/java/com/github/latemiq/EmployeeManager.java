18c4c0c8a1b136bdb2067df808f0d5b016b73349
latemiq <dzierzkowskij@gmail.com>
2023-05-17 13:15:44 +0200

    ADD-EMPLOYEE-UPDATE-FUNCTION

--- src/main/java/com/github/latemiq/EmployeeManager.java
+++ src/main/java/com/github/latemiq/EmployeeManager.java
@@ -19,4 +19,20 @@ import java.util.HashMap;
             else
                 System.out.println("Pracownik o takim ID nie istnieje");
         }
-}
+        public void updateEmployee(int employeeID, Employee newEmployee){
+            if(employees.containsKey(employeeID))
+            {
+                employees.put(employeeID,newEmployee);
+            }
+            else
+                System.out.println("Pracownik o takim ID nie istnieje");
+        }
+        public Employee searchEmployeeByID(int employeeID) {
+            if(employees.containsKey(employeeID)){
+                return employees.get(employeeID);
+            }
+            else
+                System.out.println("Nie znaleziono pracownika o danym ID");
+            return null;
+        }
+    }
