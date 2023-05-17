f78e121a946018269736e47dad4d09ceb10bf25b
latemiq <dzierzkowskij@gmail.com>
2023-05-17 13:16:15 +0200

    ADD-EMPLOYEE-SEARCH-FUNCTION

--- src/main/java/com/github/latemiq/EmployeeManager.java
+++ src/main/java/com/github/latemiq/EmployeeManager.java
@@ -1,8 +1,11 @@
 package com.github.latemiq;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
-    public class EmployeeManager {
+public class EmployeeManager {
         private final HashMap<Integer,Employee> employees;
 
         public EmployeeManager(HashMap<Integer, Employee> employees) {
@@ -35,4 +38,22 @@ import java.util.HashMap;
                 System.out.println("Nie znaleziono pracownika o danym ID");
             return null;
         }
+    public void listAllEmployees(){
+        ArrayList<Employee> employeeList = new ArrayList<>(employees.values());
+        for (Employee employee: employeeList
+        ) {
+            System.out.println(employee);
+        }
+    }
+    public HashSet<Employee> searchEmployeesByPosition(String position){
+        HashSet<Employee> employeeList = new HashSet<>(employees.values());
+        Iterator<Employee> employeeIterator = employeeList.iterator();
+        while(employeeIterator.hasNext())
+        {
+            Employee employee = employeeIterator.next();
+            if(!employee.getPosition().equals(position))
+                employeeIterator.remove();
+        }
+        return employeeList;
+    }
     }
